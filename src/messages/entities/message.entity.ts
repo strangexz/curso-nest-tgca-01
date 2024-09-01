ccce7abf2a946585342422788000075bd603e2e4
Strangexz <ja_figueroa@outlook.es>
2024-09-01 12:56:06 -0600

    feat: entidad de mensajes

---
+++ src/messages/entities/message.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nick: string;
+
+  @Column()
+  message: string;
+}
