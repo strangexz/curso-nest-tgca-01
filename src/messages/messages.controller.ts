0b2f0c44cccfd2cc8ed0da9b52074a9922de455c
Strangexz <ja_figueroa@outlook.es>
2024-09-01 12:56:42 -0600

    feat: controlador de mensajes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,27 +1,84 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message-dto';
+import { MessageService } from './services/message/message.service';
+import { Response } from 'express';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(private messageService: MessageService) {}
+
   @Post()
-  create(@Body() createMessageDto: CreateMessageDto) {
+  create(
+    @Body() createMessageDto: CreateMessageDto,
+    @Res() response: Response,
+  ) {
     console.log(createMessageDto);
-    return 'Mensaje creado';
+    this.messageService
+      .createMessage(createMessageDto)
+      .then((message) => {
+        response.status(HttpStatus.CREATED).json(message);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Error al crear el mensaje' });
+      });
   }
 
   @Get()
-  getAll() {
-    return 'Lista de mensajes';
+  getAll(@Res() response: Response) {
+    this.messageService
+      .getAll()
+      .then((messageList) => {
+        response.status(HttpStatus.OK).json(messageList);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Error en la obtención de los mensajes' });
+      });
   }
 
   @Put(':id')
-  update(@Body() updateMessageDto: CreateMessageDto) {
+  update(
+    @Body() updateMessageDto: CreateMessageDto,
+    @Res() response: Response,
+    @Param('id') idMessage: number,
+  ) {
     console.log(updateMessageDto);
-    return 'Mensaje actualizado';
+    this.messageService
+      .updateMessage(idMessage, updateMessageDto)
+      .then((message) => {
+        return response.status(HttpStatus.OK).json(message);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Error en la edición del mensaje' });
+      });
   }
 
   @Delete(':id')
-  delete() {
-    return 'Mensaje eliminado';
+  delete(@Res() response: Response, @Param('id') idMessage: number) {
+    this.messageService
+      .deleteMessage(idMessage)
+      .then((result) => {
+        return response.status(HttpStatus.OK).json(result);
+      })
+      .catch(() => {
+        response
+          .status(HttpStatus.BAD_REQUEST)
+          .json({ message: 'Error en la eliminación del mensaje' });
+      });
   }
 }
