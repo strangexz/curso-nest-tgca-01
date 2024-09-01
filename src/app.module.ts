235c70d9e67c38b295feea65999d294301a326ef
Strangexz <ja_figueroa@outlook.es>
2024-09-01 12:55:38 -0600

    feat: configuracion de la conexion a la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesController } from './messages/messages.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Message } from './messages/entities/message.entity';
+import { MessageService } from './messages/services/message/message.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'cursos_tgca_nest_db',
+      entities: [Message],
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Message]),
+  ],
   controllers: [AppController, MessagesController],
-  providers: [AppService],
+  providers: [AppService, MessageService],
 })
 export class AppModule {}
