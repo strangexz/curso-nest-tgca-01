ea5c77479d9db5102313c4d1725b92fff231ff05
Strangexz <ja_figueroa@outlook.es>
2024-09-01 10:41:01 -0600

    refactor: metodos provisionales del controlador

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message-dto';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Post()
+  create(@Body() createMessageDto: CreateMessageDto) {
+    console.log(createMessageDto);
+    return 'Mensaje creado';
+  }
+
+  @Get()
+  getAll() {
+    return 'Lista de mensajes';
+  }
+
+  @Put(':id')
+  update(@Body() updateMessageDto: CreateMessageDto) {
+    console.log(updateMessageDto);
+    return 'Mensaje actualizado';
+  }
+
+  @Delete(':id')
+  delete() {
+    return 'Mensaje eliminado';
+  }
+}
